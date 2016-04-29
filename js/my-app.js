1d45a075b7127f97883c9bc1ab0c951c4fba232c
EbbAndFlow <EllisCollinson@live.com>
2016-04-29 22:04:44 +0100

    Stable
    Still lacking writing functionality

--- js/my-app.js
+++ js/my-app.js
@@ -17,29 +17,69 @@ var $$ = Dom7;
 
 //new below
 
+var todoData = localStorage.td7Data ? JSON.parse(localStorage.td7Data) : [];
+
+$$('.popup').on('open', function () {
+    $$('body').addClass('with-popup');
+});
+$$('.popup').on('opened', function () {
+    $$(this).find('input[name="title"]').focus();
+});
+$$('.popup').on('close', function () {
+    $$('body').removeClass('with-popup');
+    $$(this).find('input[name="title"]').blur().val('');
+});
+
+// Popup colors
+$$('.popup .color').on('click', function () {
+    $$('.popup .color.selected').removeClass('selected');
+    $$(this).addClass('selected');
+});
+
+// Add Task
+$$('.popup .add-task').on('click', function () {
+    var title = $$('.popup input[name="title"]').val().trim();
+    if (title.length === 0) {
+        return;
+    }
+    var color = $$('.popup .color.selected').attr('data-color');
+    todoData.push({
+        title: title,
+        color: color,
+        checked: '',
+        id: (new Date()).getTime()
+    });
+    localStorage.td7Data = JSON.stringify(todoData);
+    buildTodoListHtml();
+    myApp.closeModal('.popup');
+});
+
+
+//NOT WORKING
+
 $$('.get-storage-data').on('click', function() {
- var storedData = myApp.formGetData('my-form2');
- if(storedData) {
-   alert(JSON.stringify(storedData));
- }
- else {
-   alert('There is no stored data for this form yet. Try to change any field')
- }
+  var storedData = myApp.formGetData('my-form2');
+  if(storedData) {
+    alert(JSON.stringify(storedData));
+  }
+  else {
+    alert('There is no stored data for this form yet. Try to change any field')
+  }
 });
 
 $$('.delete-storage-data').on('click', function() {
- var storedData = myApp.formDeleteData('my-form2');
- alert('Form data deleted')
+  var storedData = myApp.formDeleteData('my-form2');
+  alert('Form data deleted')
 });
 
 $$('.save-storage-data').on('click', function() {
- var storedData = myApp.formStoreData('my-form2', {
-   'name': 'John',
-   'date': 'john@doe.com',
-   'inspection': ['yes'],
-   'notes': 'I am the Tide',
- });
- alert('Form data replaced, refresh browser to see changes')
+  var storedData = myApp.formStoreData('my-form2', {
+    'name': 'John',
+    'date': 'john@doe.com',
+    'inspection': ['yes'],
+    'notes': 'I am the Tide',
+  });
+  alert('Form data replaced, refresh browser to see changes')
 });
 
 
