5245c6429091c9b312db1c62756dc9212ee68559
EbbAndFlow <EllisCollinson@live.com>
2016-04-29 17:59:50 +0100

    About Page Complete
    The about page has been complete!

--- js/my-app.js
+++ js/my-app.js
@@ -15,6 +15,34 @@ var myApp = new Framework7({
 // Export selectors engine
 var $$ = Dom7;
 
+//new below
+
+$$('.get-storage-data').on('click', function() {
+ var storedData = myApp.formGetData('my-form2');
+ if(storedData) {
+   alert(JSON.stringify(storedData));
+ }
+ else {
+   alert('There is no stored data for this form yet. Try to change any field')
+ }
+});
+
+$$('.delete-storage-data').on('click', function() {
+ var storedData = myApp.formDeleteData('my-form2');
+ alert('Form data deleted')
+});
+
+$$('.save-storage-data').on('click', function() {
+ var storedData = myApp.formStoreData('my-form2', {
+   'name': 'John',
+   'date': 'john@doe.com',
+   'inspection': ['yes'],
+   'notes': 'I am the Tide',
+ });
+ alert('Form data replaced, refresh browser to see changes')
+});
+
+
 // Defined as function "getrandom"
 function loadParts() {
   $$.getJSON('parts.json', function (json) {
